00c4ea1128e7c706c30ad39fda59f49fc309aef3
ARIEL <ariel.salvia78@gmail.com>
2024-04-06 08:54:21 -0300

    commit 2

--- App.js
+++ App.js
@@ -1,5 +1,7 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { Navigation } from 'react-native-navigation';
+
 
 export default function App() {
   return (
@@ -9,6 +11,44 @@ export default function App() {
     </View>
   );
 }
+
+
+
+const createBottomTabNavigator = () => {
+  Navigation.registerComponent('ZapatillasScreen', () => ZapatillasScreen);
+  Navigation.registerComponent('RopaScreen', () => RopaScreen);
+  Navigation.registerComponent('AccesoriosScreen', () => AccesoriosScreen);
+
+  Navigation.setRoot({
+    root: {
+      bottomTabs: {
+        children: [
+          {
+            component: {
+              name: 'ZapatillasScreen',
+            },
+            icon: require('./assets/zapatillas.png'),
+            label: 'Zapatillas',
+          },
+          {
+            component: {
+              name: 'RopaScreen',
+            },
+            icon: require('../assets/ropa.png'),
+            label: 'Ropa',
+          },
+          {
+            component: {
+              name: 'AccesoriosScreen',
+            },
+            icon: require('../assets/accesorios.png'),
+            label: 'Accesorios',
+          },
+        ],
+      },
+    },
+  });
+};
 
 const styles = StyleSheet.create({
   container: {
