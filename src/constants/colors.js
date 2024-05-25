c3a0d1632a8999591fc1945aa974e42642215d4a
ARIEL <ariel.salvia78@gmail.com>
2024-05-25 14:41:09 -0300

    commit

---
+++ src/constants/colors.js
@@ -0,0 +1,7 @@
+export const colors = {
+    teal900: '#562C2C',
+    teal600: '#F2542D',
+    teal400: '#F5DFBB',
+    teal200: '#0E9594',
+    platinum: '#E0E1DD'
+}
---
+++ src/screens/Home.jsx
@@ -0,0 +1,41 @@
+import { FlatList, StyleSheet, Text, View } from "react-native";
+import { colors } from "../constants/colors";
+import CategoryItem from "../components/CategoryItem";
+import Counter from "../components/Counter";
+import { useGetCategoriesQuery } from "../services/shopService";
+
+const Home = ({ route, navigation}) => {
+  const {data: categories, error, isLoading} = useGetCategoriesQuery()
+
+
+  return (
+    <View style={styles.flatListContainer}>
+      {/* <Counter/> */}
+      <FlatList
+        showsVerticalScrollIndicator={false}
+        keyExtractor={(elemntoDeMiArray) => elemntoDeMiArray}
+        data={categories}
+        renderItem={({ item }) => (
+          <CategoryItem 
+            navigation={navigation} 
+            category={item} 
+          />
+        )}
+      />
+    </View>
+  )
+}
+
+export default Home
+
+const styles = StyleSheet.create({
+  flatListContainer: {
+    width: "100%",
+    backgroundColor: colors.teal400,
+    flex: 1,
+    flexDirection: "column",
+    justifyContent: "center",
+    alignItems: "center",
+    padding: 10,
+  },
+})
