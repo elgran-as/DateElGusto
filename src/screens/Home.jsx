d6ee50384161b3c72f6d08fdb0bb6bcc2b78b959
ARIEL <ariel.salvia78@gmail.com>
2024-05-26 00:13:13 -0300

    4

--- src/screens/Home.jsx
+++ src/screens/Home.jsx
@@ -1,35 +1,43 @@
-import { FlatList, StyleSheet, Text, View } from "react-native";
+
+import { FlatList, StyleSheet, Text, View, ImageBackground } from "react-native";
 import { colors } from "../constants/colors";
 import CategoryItem from "../components/CategoryItem";
 import { useGetCategoriesQuery } from "../services/shopService";
 
-const Home = ({ route, navigation}) => {
-  const {data: categories, error, isLoading} = useGetCategoriesQuery()
-
+const Home = ({ route, navigation }) => {
+  const { data: categories, error, isLoading } = useGetCategoriesQuery();
 
   return (
-    <View style={styles.flatListContainer}>
-      {/* <Counter/> */}
-      <FlatList
-        showsVerticalScrollIndicator={false}
-        keyExtractor={(elemntoDeMiArray) => elemntoDeMiArray}
-        data={categories}
-        renderItem={({ item }) => (
-          <CategoryItem 
-            navigation={navigation} 
-            category={item} 
-          />
-        )}
-      />
-    </View>
-  )
-}
+    <ImageBackground source={{ uri: 'https://thumbs.dreamstime.com/z/fondo-de-cocina-con-mesa-madera-blanca-alimentos-marco-comida-sobre-verduras-frescas-especias-hierbas-y-aceite-ingredientes-para-187070339.jpg' }} style={styles.backgroundImage}>
+      <View style={styles.contentContainer}>
+        <FlatList
+          showsVerticalScrollIndicator={false}
+          keyExtractor={(element) => element.id}
+          data={categories}
+          renderItem={({ item }) => (
+            <CategoryItem
+              navigation={navigation}
+              category={item}
+            />
+          )}
+        />
+      </View>
+    </ImageBackground>
+  );
+};
 
-export default Home
+export default Home;
 
 const styles = StyleSheet.create({
-  flatListContainer: {
-    width: "100%",
+  backgroundImage: {
+    flex: 1,
+    width: '100%',
+    height: '100%',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  contentContainer: {
+    width: '100%',
     backgroundColor: colors.teal400,
     flex: 1,
     flexDirection: "column",
@@ -37,4 +45,4 @@ const styles = StyleSheet.create({
     alignItems: "center",
     padding: 10,
   },
-})
+});
