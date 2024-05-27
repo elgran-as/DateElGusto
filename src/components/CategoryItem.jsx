bad4321555610d1d240598d4b5464e0f53523247
AAS DEV <134068926+elgran-as@users.noreply.github.com>
2024-05-27 10:29:38 -0300

    Update CategoryItem

--- src/components/CategoryItem.jsx
+++ src/components/CategoryItem.jsx
@@ -15,9 +15,11 @@ const CategoryItem = ({ category, navigation }) => {
   }
 
   return (
-    <Card style={{ marginVertical: 10, marginHorizontal: 10, borderColor: 'rgba (255, 165, 0, 0.5)' }}>
-      <Pressable onPress={handleNavigate}>
-        <Text style={styles.text}>{category}</Text>
+     <Card style={{ marginVertical: 10, marginHorizontal: 10, borderColor: 'rgba(255, 165, 0, 0.5)', borderWidth: 1 }}>
+      <Pressable onPress={handleNavigate} style={styles.cardContainer}>
+        <View style={styles.circleContainer}>
+          <Text style={styles.text}>{category}</Text>
+        </View>
       </Pressable>
     </Card>
   )
@@ -26,6 +28,22 @@ const CategoryItem = ({ category, navigation }) => {
 export default CategoryItem
 
 const styles = StyleSheet.create({
+  cardContainer: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  circleContainer: {
+    position: 'absolute',
+    top: 0,
+    left: 0,
+    right: 0,
+    bottom: 0,
+    justifyContent: 'center',
+    alignItems: 'center',
+    borderRadius: 50,
+    backgroundColor: colors.platinum,
+  },
   text: {
     color: colors.platinum,
     textAlign: "center",
